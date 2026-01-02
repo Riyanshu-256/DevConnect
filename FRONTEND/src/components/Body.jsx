006850b45aefabed22eae0d2dfcac580968514a0
Anshu <riyanshu062@gmail.com>
2026-01-02 20:59:29 +0530

    CORS bug fixed

--- FRONTEND/src/components/Body.jsx
+++ FRONTEND/src/components/Body.jsx
@@ -1,13 +1,45 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import { useEffect } from "react";
+
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get({ BASE_URL } + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(res.data));
+    } catch (err) {
+      dispatch(removeUser());
+
+      // ✅ redirect only if not already on login/signup
+      if (location.pathname !== "/login" && location.pathname !== "/signup") {
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div>
+    <>
       <NavBar />
       <Outlet />
       <Footer />
-    </div>
+    </>
   );
 };
 
